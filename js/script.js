1ed9f0066908de698a897e3151df752f9f351474
ivantsov1997 <ivantsov1997@mail.ru>
2023-10-31 01:35:27 +0500

    gallery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
-function myFunction(imgs) {
-    var expandImg = document.getElementById("expandedImg");
-    var imgText = document.getElementById("imgtext");
-    expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
-    expandImg.parentElement.style.display = "block";
+const imgSelect = document.querySelector('#gallery-selected')
+const imgPrev = document.querySelector('#gallery-previews')
+const imgs = [
+    '../img/1.jpg',
+    '../img/2.jpg',
+    '../img/3.jpg',
+    '../img/4.jpg'
+]
+const currentSrc = imgSelect.getAttribute('src')
+const currentSrcIndex = imgs.indexOf(currentSrc)
+imgSelect.setAttribute('src', imgs[0])
+
+for(let img in imgs){
+    const previewItem = document.createElement('img')
+    previewItem.setAttribute('src', imgs[img])
+    previewItem.classList.add('gallery-preview-item')
+    if(img == 0 ){
+        previewItem.classList.add('gallery-preview-item-select')
+    }
+    previewItem.addEventListener('click', e =>{
+        const previewItems = document.querySelectorAll('.gallery-preview-item')
+        previewItems.forEach(item=>item.classList.remove('gallery-preview-item-select'))
+        previewItem.classList.add('gallery-preview-item-select')
+        imgSelect.setAttribute('src', imgs[img])
+
+    } )
+    imgPrev.appendChild(previewItem)
 }
+
